f63f7d9a3588452d7a8f4403e8ae1a5f2261bf4d
Ramin Mtmd <dipper.mort2002@gmail.com>
2025-11-08 19:44:14 +0100

    Styled homepage and enlarged profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,39 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <img src="/profile.jpg" alt="Ramin" className="profile-pic" />
-      <h1>Ramin Motamed</h1>
-      <p>Web Developer • Game Enthusiast</p>
-      <div>
-        <a href="https://github.com/Ramin20021381">GitHub</a>
-        <a href="https://t.me/Ramin2002">Telegram</a>
-        <a href="https://www.instagram.com/ramin_mtmd/?__pwa=1">Instagram</a>
+    <div className="main-container">
+      {/* بخش بالا */}
+      <div className="top-section">
+        <img src="/profile.jpg" alt="Ramin" className="profile-pic" />
+
+        <h1 className="name">Ramin Motamed</h1>
+        <p className="subtitle">Web Developer • Game Enthusiast</p>
+
+        <div className="links">
+          <a href="https://github.com/Ramin20021381" target="_blank" className="btn github">
+            GitHub
+          </a>
+          <a href="https://t.me/Ramin2002" target="_blank" className="btn telegram">
+            Telegram
+          </a>
+          <a href="https://www.instagram.com/ramin_mtmd/?__pwa=1" target="_blank" className="btn instagram">
+            Instagram
+          </a>
+        </div>
+      </div>
+
+      {/* بخش پایین */}
+      <div className="bottom-section">
+        <h2 className="interests-title">Interested in:</h2>
+        <ul className="interests-list">
+          <li>🎮 Game Development</li>
+          <li>💻 Web Design & Web Developing</li>
+          <li>HTML - Css - JavaScript -TypeScript - React</li>
+          <li>🎧 Sound Design & Audio Processing</li>
+        </ul>
       </div>
     </div>
   );
 }
-
